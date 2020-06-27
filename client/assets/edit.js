b74e7ec5bec981ef14e3617138b174f1995016f1
TS22082 <ts22082@gmail.com>
2020-06-27 13:19:12 -0700

    write functionalty for edit

---
+++ client/assets/edit.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
+
+  $.ajax({
+    type: "GET",
+    url: `/api/find/${id}`,
+  }).then((todo) => {
+    console.log(todo);
+
+    todo[0].completed
+      ? $("#completedCheck").prop("checked", true)
+      : $("#completedCheck").prop("checked", false);
+
+    $("#editTextInput").prop("placeholder", todo[0].text);
+  });
+
+  $("#btnConfirm").on("click", (e) => {
+    e.preventDefault();
+
+    const editedText = $("#editTextInput").val()
+      ? $("#editTextInput").val()
+      : $("#editTextInput").prop("placeholder");
+
+    const completeStatus = $("#completedCheck").prop("checked")
+      ? "true"
+      : "false";
+  });
+});
